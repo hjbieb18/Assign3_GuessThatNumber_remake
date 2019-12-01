9600e3ab235df93d22f16150fa3a8b1898da5043
Holden <hjbieb18@smumn.edu>
2019-11-30 21:14:46 -0600

    Score Needs to implement

--- Guess_jquery.js
+++ Guess_jquery.js
@@ -1,16 +1,15 @@
-var number= Math.floor(Math.random() * 100 + 1);
-number= numberPicker();
+
 console.log(numberPicker());
 var guess= 0;
-guess= $('#guess-log');
-var score=0;
+var score= 0;
 var output = "Submit a number by clicking guess!";
 $('#feedback').text(output);
 
 
 
 $(function (){
-    $('#guess-button').on('click', guessChecker);
+    $('#guess-button').on('click', guessChecker); 
+    //score= $('#score-log').html(0); 
     $('#new-game').on('click', newGame);
 });
 
@@ -25,32 +24,33 @@ function numberPicker(){
 function guessChecker () {
     guess= parseFloat($('#guess').val());
     output="Submit a number by clicking guess!";
-    
     userGuessLog={};
+    scoreCount = 0;
+    score = $('#score-log').html(scoreCount);
     
-
-
     if (guess > number){
         output= "Guess is too high.";
         $('#guess').val("");
-        
+        score= $('#score-log').html(scoreCount + 1);
     }
     else if(guess < number){
         output= "That guess is too low.";
         $('#guess').val("");
         
+        
 
     }
     else if(guess == number){
         output="Correct guess! Click New Game to play again";
+        
     }
     $('#feedback').text(output);
     console.log(guess);
-    score++;
     parseFloat($('#guess-log').text(guess));
+}
     
      
-}
+
 function newGame() {
     window.location.reload();
     number = numberPicker();
